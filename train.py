32d2ef1129a391e22effec426b304e48493e20c9
Akshaya Balaji <akshayavb99@gmail.com>
2020-05-25 23:36:21 +0530

    new commit

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import pandas as pd
 import keras
 import os
 
-dataset=pd.read_csv("dl_models/train.csv")
+dataset=pd.read_csv("root/deeplearning/train.csv")
 dataset.head(10)
 
 #To use data from the DataFrame for DL models, we convert the data into numpy arrays
